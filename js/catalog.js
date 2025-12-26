c34c7efff7a2b99ed0b012b937fbf281bdf6aa2e
Aleksei <a.toparau@andersenlab.com>
2025-12-26 13:33:50 +0300

    refactor: catalog page

--- js/catalog.js
+++ js/catalog.js
@@ -15,3 +15,30 @@ const tabsSwiper = new Swiper('#catalog__navigation-swiper', {
     },
   },
 });
+
+//filterModal
+const filterButton = document.querySelector('.catalog__filter-button');
+const filter = document.querySelector('.catalog__filter');
+
+filterButton?.addEventListener('click', () => {
+  document.body.classList.add('filter-open');
+});
+
+document.addEventListener('click', (e) => {
+  if (
+    document.body.classList.contains('filter-open') &&
+    !filter.contains(e.target) &&
+    !e.target.closest('.catalog__filter-button')
+  ) {
+    document.body.classList.remove('filter-open');
+  }
+
+  if (
+    document.body.classList.contains('filter-open') &&
+    e.target === filter &&
+    e.offsetX > filter.offsetWidth - 50 &&
+    e.offsetY < 50
+  ) {
+    document.body.classList.remove('filter-open');
+  }
+});
