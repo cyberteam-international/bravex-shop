2ab8f471f1eee9307e9c622c77351947feb39c88
Donatello <daniilbortvin@gmail.com>
2026-02-03 12:46:09 +0700

    чейндж инпута

--- src/js/inputNumber.js
+++ src/js/inputNumber.js
@@ -1,30 +1,30 @@
 function initNumberInput() {
-  const numberInputWrapper = document.querySelector('.number-input');
+  const numberInputWrapper = document.querySelector(".number-input");
   if (!numberInputWrapper) return; // Выход если элемент не найден
 
   const numberInput = numberInputWrapper.querySelector('input[type="number"]');
-  const numberInputUp = numberInputWrapper.querySelector('.up');
-  const numberInputDown = numberInputWrapper.querySelector('.down');
+  const numberInputUp = numberInputWrapper.querySelector(".up");
+  const numberInputDown = numberInputWrapper.querySelector(".down");
 
   if (!numberInput || !numberInputUp || !numberInputDown) return; // Выход если нет необходимых элементов
 
-  numberInputUp.addEventListener('click', () => {
+  numberInputUp.addEventListener("click", () => {
     let max = numberInput.max ? parseInt(numberInput.max) : Infinity;
     let step = numberInput.step ? parseInt(numberInput.step) : 1;
     let value = parseInt(numberInput.value) || 0;
     if (value + step <= max) {
       numberInput.value = value + step;
-      numberInput.dispatchEvent(new Event('input'));
+      numberInput.dispatchEvent(new Event("input"));
     }
   });
 
-  numberInputDown.addEventListener('click', () => {
+  numberInputDown.addEventListener("click", () => {
     let min = numberInput.min ? parseInt(numberInput.min) : -Infinity;
     let step = numberInput.step ? parseInt(numberInput.step) : 1;
     let value = parseInt(numberInput.value) || 0;
     if (value - step >= min) {
       numberInput.value = value - step;
-      numberInput.dispatchEvent(new Event('input'));
+      numberInput.dispatchEvent(new Event("input"));
     }
   });
 }
